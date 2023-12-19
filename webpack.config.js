0ec9b056fe50d06418f8e4d04029ab6fad010fef
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-12-19 23:14:22 +0300

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -69,20 +69,20 @@ module.exports = () => {
     // plugins: [new CleanWebpackPlugin()],
 
     externals: {
-      // react: "React",
-      // "react-dom": "ReactDOM",
-      react: {
-        root: "React",
-        commonjs2: "react",
-        commonjs: "react",
-        amd: "react",
-      },
-      "react-dom": {
-        root: "ReactDOM",
-        commonjs2: "react-dom",
-        commonjs: "react-dom",
-        amd: "react-dom",
-      },
+      react: "react",
+      "react-dom": "react-dom",
+      // react: {
+      //   root: "React",
+      //   commonjs2: "react",
+      //   commonjs: "react",
+      //   amd: "react",
+      // },
+      // "react-dom": {
+      //   root: "ReactDOM",
+      //   commonjs2: "react-dom",
+      //   commonjs: "react-dom",
+      //   amd: "react-dom",
+      // },
     },
   };
 
