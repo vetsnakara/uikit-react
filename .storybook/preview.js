2af5fdc87d3ca90b78d3a2915c410171379bac36
Konstantin Arakantsev <k.arakantsev@bftcom.com>
2024-01-06 23:28:17 +0300

    fix paths

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,17 @@
 import { initialize, mswLoader } from "msw-storybook-addon";
 
-initialize();
+const mswOptions = {
+  serviceWorker: {
+    url: "mockServiceWorker.js", // relative path
+    // process.env.NODE_ENV === "production"
+    //   ? "mockServiceWorker.js" // relative path
+    //   : "/mockServiceWorker.js",
+  },
+};
+
+// console.log("mswOptions", mswOptions);
+
+initialize(mswOptions);
 
 const preview = {
   parameters: {
