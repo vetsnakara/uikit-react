082260eb054ebde29dfdd8543632002cf98ec21c
Konstantin Arakantsev <k.arakantsev@bftcom.com>
2024-01-07 19:10:05 +0300

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         browser: true,
+        node: true,
         jquery: true,
     },
     globals: {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
 export * from "./components";
-
-const a = 1;
-console.log(a);
